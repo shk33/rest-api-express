a195420945822f9d5957fe6a765edceaf7fc3a61
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-14 17:16:08 -0500

    Add check updated object and remove object tests

--- express.test.js
+++ express.test.js
@@ -53,4 +53,28 @@ describe('express rest api server', function () {
         done();
       });
   });
+
+  it('checks an updated object', function (done) {
+    superagent.get('http://localhost:3000/collections/test/'+id)
+      .end(function (err, res) {
+        expect(err).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql('Peter');
+        expect(res.body.email).to.eql('peter@yahoo.com');
+        done();
+      });
+  });
+
+  it('removes an object', function (done) {
+    superagent.del('http://localhost:3000/collections/test/'+id).
+      end(function (err, res) {
+        expect(err).to.eql(null);
+        expect(typeof res.body).to.eql('object');
+        expect(res.body.message).to.eql('success');
+        done();
+      });
+  });
+
 });
